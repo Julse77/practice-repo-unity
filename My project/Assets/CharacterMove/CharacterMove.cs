da4e0877a487e12d831ba54184ee97e3063cc1d6
Julse77 <wrs0707@gmail.com>
2022-03-29 18:46:03 +0900

    Character_move
    - Jump()
    - AddForce

--- My project/Assets/CharacterMove/CharacterMove.cs
+++ My project/Assets/CharacterMove/CharacterMove.cs
@@ -5,24 +5,44 @@ using UnityEngine;
 public class CharacterMove : MonoBehaviour
 {
     public float moveSpeed = 10f;       // 이동속도
-    private Rigidbody characterRigidbody;
+    public float jumpForce = 5f;        // 점프힘
+    private bool isJumping;
+    private Rigidbody playerRigidbody;
 
     void Start()
     {
-        characterRigidbody = GetComponent<Rigidbody>();        
+        playerRigidbody = GetComponent<Rigidbody>();        // Player에서 rigidbody 가져옴
     }
 
     void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
     {
         // -1 ~ 1 입력
         float horizontalInput = Input.GetAxis("Horizontal");    // 좌우
         float verticalInput = Input.GetAxis("Vertical");        // 상하
 
-        float fallSpeed = characterRigidbody.velocity.y;        // 떨어지는 속도
+        float fallSpeed = playerRigidbody.velocity.y;        // 떨어지는 속도
 
         Vector3 velocity = new Vector3(horizontalInput, 0, verticalInput);
         velocity *= moveSpeed;
         velocity.y = fallSpeed;     // 떨어지는 속도 초기화
-        characterRigidbody.velocity = velocity;
+        playerRigidbody.velocity = velocity;
     }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("점프");
+            playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+        }
+    }
+
+
 }
